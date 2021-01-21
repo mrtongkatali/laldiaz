2b723ed094a94ebed80a8d5563616fd76ade12e2
Leo Diaz <leo@crazyapp.cloud>
2021-01-21 23:39:45 +0800

    Add portfolio urls

--- landing_v1/views.py
+++ landing_v1/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'landing_v1/index.html')
+    urls = {
+        "cv": "https://s3-ap-southeast-1.amazonaws.com/laldiaz.me/LeoDiaz-Resume-2021-v1.pdf",
+        "gitlab": "https://gitlab.com/mrtongkatali",
+        "linkedin": "https://www.linkedin.com/in/mrtongkatali/",
+        "github": "https://github.com/mrtongkatali",
+        "email": "leo@crazyapp.cloud",
+        "mailto_href": "mailto:leo@crazyapp.cloud",
+        "cookery_api": "http://api-dev-cookery.crazyapp.cloud/api/swagger",
+        "mcl": "https://mcl.edu.ph/"
+
+    }
+
+    return render(request, 'landing_v1/index.html', urls)
